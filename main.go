5b004dd07d26f94b9a1bd7b3a5526ddf27e6fa0d
Dennis Mattews <alttert@mail.ru>
2024-07-14 16:47:50 +0300

    learning golang the normal way

--- main.go
+++ main.go
@@ -8,6 +8,105 @@ import (
 Per meliora
 ad astra
 et altiora!
+
+
+//firstNum := int
+//secondNum := int
+
+fmt.Scan(&firstNum)
+fmt.Scan(&secondNum)
+
+add := "+"
+subtract := "-"
+multiply := "*"
+divide := "/"
+modulo := "%"
+
+================================================
+var a, b int
+fmt.Scan(&a)
+fmt.Scan(&b)
+if a > b; a > 1000 {
+    fmt.Println("a больше чем b")
+} else if a == b {
+    fmt.Println("a равно b")
+} else {
+    fmt.Println("a меньше чем b")
+}
+
+================================================
+height := 177
+if height <= 165 {
+     fmt.Println("Человек невысокого роста")
+} else if height > 165 && height <= 185 {
+     fmt.Println("Человек среднего роста")
+} else {
+     fmt.Println("Высокий человек")
+}
+// выведет: Человек среднего роста
+================================================
+month := "Январь"
+
+switch {
+    case month == "Декабрь" || month == "Январь" || month == "Февраль":
+        fmt.Println("Зима")
+    case month == "Март" || month == "Апрель" || month == "Май":
+        fmt.Println("Весна")
+}
+
+================================================
+if result = add:
+	return firstNum + secondNum
+	fmt.Println(result)
+
+if result = subtract:
+	return firstNum - secondNum
+	fmt.Println(result)
+
+if result = multiply:
+	return firstNum * secondNum
+	fmt.Println(result)
+
+if result = divide:
+	return firstNum / secondNum
+		if firstNum and secondNum != float32:
+			if secondNum := 0:
+				Division by zero is not allowed
+	fmt.Println(result)
+
+if result = modulo:
+	return firstNum % secondNum
+	fmt.Println(result)
+
+fmt.Println(result)
+
+var a int = 31
+var b int = 14
+--------------
+a += b
+fmt.Println(a)  // выведет 45
+--------------
+a -= b
+fmt.Println(a)  // выведет 17
+--------------
+a *= b
+fmt.Println(a)  // выведет 434
+--------------
+a /= b
+fmt.Println(a)  // выведет 2
+--------------
+a %= b
+fmt.Println(a)  // выведет 3
+
+
+func mainSecond() {
+    var x int
+    var y int
+    fmt.Scanln(&x)     // считаем первое число в переменную x
+    fmt.Scanln(&y)     // считаем второе число в переменную y
+    fmt.Println(x + y) // выведем сумму чисел - 12
+}
+
 */
 
 func main() {
