8187cf15f603784e1dfc73ac79830fdb50b5e526
Dennis Mattews <alttert@mail.ru>
2024-07-20 17:41:03 +0300

    main func

--- main.go
+++ main.go
@@ -6,11 +6,6 @@ import (
 )
 
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Panic:", r)
-		}
-	}()
 	calculator.MainCalc()
 	fmt.Println("Thank you for using our Golang Calculator! ^w^")
 }
