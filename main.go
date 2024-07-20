cc89e5124b76fbf101de233032640802522e6bd3
Dennis Mattews <alttert@mail.ru>
2024-07-21 00:35:27 +0300

    calc main.go upd: defer func raised to top

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func main() {
 
 	for {
 		fmt.Println("Введите выражение (или введите 'exit' для выхода):")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input) // Удаляем пробелы в начале и конце строки
+		input, _ := reader.ReadString('\n') // Читаем строку
+		input = strings.TrimSpace(input)    // Удаляем префиксные и постфиксные пробелы в начале и конце строки
 
 		if strings.ToLower(input) == "exit" {
-			break // Завершаем работу программы, если введено 'exit'
+			break // Завершаем работу программы, если введено 'exit', выходим из цикла
 		}
 
 		// Вызываем функцию для вычисления выражения и обрабатываем возможные ошибки
